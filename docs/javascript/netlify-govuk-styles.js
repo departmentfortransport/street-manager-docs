7a54fa15ad66746efceab7e0659e19b0a5270009
Steven Alexander <s.alexander@kainos.com>
2019-11-25 13:34:19 +0100

    updated javascript used to append govuk styles

--- docs/javascript/netlify-govuk-styles.js
+++ docs/javascript/netlify-govuk-styles.js
@@ -1,2 +1,12 @@
 // Script to append gov.uk styles to HTML pages generated from Netlify CMS markdown
 // see https://design-system.service.gov.uk/ for styles
+$('.govuk-main-wrapper h1').addClass('govuk-heading-xl');
+$('.govuk-main-wrapper h2').addClass('govuk-heading-l');
+$('.govuk-main-wrapper h3').addClass('govuk-heading-m');
+$('.govuk-main-wrapper h4').addClass('govuk-heading-s');
+
+$('.govuk-main-wrapper p').addClass('govuk-body');
+$('.govuk-main-wrapper a').addClass('govuk-link');
+
+$('.govuk-main-wrapper ol').addClass('govuk-list');
+$('.govuk-main-wrapper ul').addClass('govuk-list govuk-list--bullet');
